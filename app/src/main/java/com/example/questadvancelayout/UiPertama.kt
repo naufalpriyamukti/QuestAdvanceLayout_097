6abad32fac271ad5e25b99231e3a0fff00c021e4
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-10-23 14:34:23 +0700

    menambahkan kolom teks nama dan alamat pengguna di dalam Row

--- app/src/main/java/com/example/questadvancelayout/UiPertama.kt
+++ app/src/main/java/com/example/questadvancelayout/UiPertama.kt
@@ -49,6 +49,21 @@ fun AktifitasPertama(modifier: Modifier) { Column(modifier = Modifier.padding(to
                 modifier = Modifier.size(100.dp).padding(5.dp)
             )
             Spacer(modifier = Modifier.width(30.dp))
+            Column() {
+                Text(
+                    stringResource(R.string.nama),
+                    fontSize = 30.sp,
+                    fontFamily = FontFamily.Cursive,
+                    color = Color.White,
+                    modifier = Modifier.padding(top = 15.dp)
+                )
+                Text(
+                    stringResource(R.string.alamat),
+                    fontSize = 20.sp,
+                    color = Color.Yellow,
+                    modifier = Modifier.padding(top = 10.dp)
+                )
+            }
         }
     }
 }
